15f78b5263f9b48f86c3209d3cb47fc2f08985e4
Cyanro <113330384+Cyanro@users.noreply.github.com>
2024-09-23 10:55:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git ADJ https://github.com/ADJazzzz/OpenWrt-package' >>feeds.conf.default
