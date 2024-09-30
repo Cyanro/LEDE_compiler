74ac3297c7c735c78f19a0dfd8089142101b9713
Cyanro <113330384+Cyanro@users.noreply.github.com>
2024-09-30 11:44:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,8 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git ADJ https://github.com/ADJazzzz/OpenWrt-package' >>feeds.conf.default
+#echo 'src-git ADJ https://github.com/ADJazzzz/OpenWrt-package' >>feeds.conf.default
+
+#add package
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/Cyanro/luci-app-vssr.git package/lean/luci-app-vssr
